0b73f4c52324fca2b8481c7efd21c39a23cd3a10
Vladimir <wtorushin@yandex.ru>
2024-08-02 20:42:20 +0300

    Init Spring

---
+++ src/test/java/com/quartztop/bonus/BonusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartztop.bonus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
